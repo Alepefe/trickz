dc9945a30ecf6e7f9aebbad46c62a70f096e4786
Alepefe <36237825+Alepefe@users.noreply.github.com>
2021-03-07 00:23:32 +0100

    Create swap.sh

---
+++ swap.sh
@@ -0,0 +1,8 @@
+fallocate -l 2G /swapfile
+dd if=/dev/zero of=/swapfile bs=1024 count=2097152
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo '/swapfile          swap            swap    defaults        0 0' >> /etc/fstab
+sysctl vm.swappiness=10
+echo 'vm.swappiness=10' >> /etc/sysctl.conf
